7c8c8e2819e9a5981a83ec4515f4e411e8a88d0c
Gianluigi Spagnuolo <spagnuolo@alturalabs.com>
2015-02-18 14:08:36 +0100

    Cleaned code

--- lib/ups_shipping/organization.rb
+++ lib/ups_shipping/organization.rb
@@ -2,16 +2,13 @@ require "nokogiri"
 
 module Shipping
   class Organization
-    attr_accessor :company_name, :name, :phone, :email, :address, :shipper_number
+    attr_accessor :company_name, :name, :phone, :email, :address
 
     def initialize(options={})
       @company_name = options[:company_name]
       @name = options[:name]
       @phone = options[:phone]
       @address = options[:address]
-      if options[:shipper_number]
-        @shipper_number = options[:shipper_number]
-      end
     end
 
     def build(xml, rootname)
@@ -19,9 +16,7 @@ module Shipping
         xml.CompanyName @company_name
         xml.AttentionName @name
         xml.PhoneNumber @phone
-        if @shipper_number
-          xml.ShipperNumber @shipper_number
-        end
+
         @address.build(xml)
       }
     end
